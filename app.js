d3235c1c21b722c26f8f94c26404ab738fddddee
himmel <himmel@gouju.com>
2013-07-17 20:18:00 +0800

    add homepage

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
+  , admin = require('./routes/admin')
   , http = require('http')
   , path = require('path');
 
@@ -31,7 +31,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/admin', admin.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '荒野的呼唤-潘文石生物多样性基金会' });
 };
