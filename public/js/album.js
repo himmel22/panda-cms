af141104b5ebe82d8576f01a49a9b4d82284674e
Himmel <himmelex@gmail.com>
2013-09-07 02:19:47 +0800

    upload

--- public/js/album.js
+++ public/js/album.js
@@ -16,35 +16,52 @@ $(function() {
 
 function upload(file) {
 
-    if (window.FormData) { 
+    if (window.FormData) {
 
         var formData = new FormData();
         formData.append("image", file);
 
-        $.ajax({  
-            url: "/upload/image",  
-            type: "POST",  
-            data: formData,  
-            processData: false,  
-            contentType: false,  
-            success: function (res) {  
+        $.ajax({
+            url: "/upload/image",
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(res) {
                 addPhoto(res.url);
-            }  
-        }); 
+            }
+        });
 
     }
 }
 
 function addPhoto(url) {
+    clearAlert();
+    var photoExist = $('#album-field input[name="album[]"][value="' + url + '"]').length;
+    if (!photoExist) {
+        //add input
+        $('<input type="hidden">').attr({
+            name: 'album[]',
+            value: url
+        }).appendTo('#album-field');
 
-    //add input
-    $('<input type="hidden">').attr({
-        name: 'album[]',
-        value: url
-    }).appendTo('#album-field');
+        //add img preview
+        var newPhoto = '<li><img src="' + url + '"><div class="delete"><a href="#">删除</a></div></li>'
+        $('#album-list .album-upload').before(newPhoto);
+    } else {
+        setAlert('已经添加过这张图片');
+    }
 
-    //add img preview
-    var newPhoto = '<li><img src="'+ url + '"><div class="delete"><a href="#">删除</a></div></li>'
-    $('#album-list .album-upload').before(newPhoto);
+}
 
+
+function setAlert(message) {
+    var alertDOM = $('#album-field .alert');
+    alertDOM.remove();
+
+    $('#album-field #album-list').prepend('<div class="alert">' + message + '</div>');
+}
+
+function clearAlert() {
+    $('#album-field .alert').remove();
 }
